98f83e54cca4193ca04055fe2cfbab2358b1e666
PavelKhromovAndrS <pavelkhromov1993@yandex.ru>
2021-10-28 14:14:32 +0300

    Added bottom navigation menu

--- app/src/main/java/com/example/notesv2/ui/MainActivity.java
+++ app/src/main/java/com/example/notesv2/ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.notesv2.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.notesv2.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_all_notes) {
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container_view, new ListNotesFragment())
+                            .commit();
+
+                    return true;
+                }
+
+                if (item.getItemId() == R.id.action_search) {
+                    Toast.makeText(MainActivity.this, R.string.toast_search, Toast.LENGTH_SHORT).show();
+
+                    return true;
+                }
+
+                if (item.getItemId() == R.id.action_settings) {
+                    Toast.makeText(MainActivity.this, R.string.toast_settings, Toast.LENGTH_SHORT).show();
+
+                    return true;
+                }
+
+                return false;
+            }
+        });
     }
 
 }
